470eb39a79c1b3baa129481d6bc162ed8ee9c1dc
shaaadman <shaaadman@yahoo.com>
2019-05-26 14:39:23 -0400

    Shadman's share code

---
+++ FirstGitProject/src/firstgitproject/ShadmanGit.java
@@ -0,0 +1,10 @@
+package firstgitproject;
+
+public class ShadmanGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is Shadman's code");
+	}
+
+}
---
+++ FirstGitProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Razer
+ *
+ */
+module FirstGitProject {
+}
